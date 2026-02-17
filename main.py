e5c964a86f922c92c60bbfc6bbe595d95a9063ca
Sam Swanger <samuel.e.swanger@gmail.com>
2026-02-17 17:17:30 +0000

    Added Pandas and set up the class

--- main.py
+++ main.py
@@ -2,5 +2,13 @@
 
 #You need to evauluate a all ahead paths not just the  beacuse you want the least value
 
-a_value = 0 #will use to count value of path
+import pandas as pd
+
+
+class Shortest_Path():
+    def __init__(self):
+        self.matrix = pd.read_table('0081_matrix.txt')
+        self.a_value = 0 #will use to count value of path
+
+
 
